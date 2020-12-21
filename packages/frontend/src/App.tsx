357dee5d59359410dd553de7a89e8a5f21132aa0
Artur Kmera <artur.kmera@3code.pl>
2020-12-21 23:58:05 +0100

    App now can handle /not-found link

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,13 +3,21 @@ import "./App.css";
 import { ShortenerPanel } from "./Shortener/ShortenerPanel";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ShortenerPanel />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                {window.location.pathname.indexOf("not-found") !== -1 ? (
+                    <div className="App-not-found">
+                        <div>Not found</div>
+
+                        <a href="/">Go to main page</a>
+                    </div>
+                ) : (
+                    <ShortenerPanel />
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
